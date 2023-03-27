f474b805e44a3706581e584758d6d4f537d71d6f
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-03-27 10:39:20 +0200

    SomeMethodsAndSomeMethods

--- project/main.py
+++ project/main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template, make_response, abort, redirect, url_for
+from flask import session, request
+from markupsafe import escape
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -10,4 +14,20 @@ def home():
 @app.route('/rank')
 def rank():
     return render_template('rank.html')
+
+@app.route('/login')
+def connection():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
+
+@app.errorhandler(401)
+def access_denied(error):
+    return render_template('access_denied.html'), 401
+
+@app.errorhandler(404)
+def access_denied(error):
+    return render_template('page_not_found.html'), 404
 
