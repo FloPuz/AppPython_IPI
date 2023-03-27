b8fe9636dcdb89fd704ed682f05ce8fd98657f98
FloPuz <fpuzenat1@gmail.com>
2023-03-27 12:32:34 +0200

    Init database

--- cheesse.sql
+++ cheesse.sql
@@ -1,26 +1,28 @@
 PRAGMA foreign_keys = ON;
-DROP TABLE userCheese IF EXISTS
-CREATE TABLE userCheese(
-    idUser INTEGER NOT NULL PRIMARY KEY,
-    idCheese INTEGER NOT NULL PRIMARY KEY,
-    CONSTRAINT idUser FOREIGN KEY(idUser) REFERENCES user(idUser)
-    CONSTRAINT idCheese FOREIGN KEY(idCheese) REFERENCES cheese(idCheese)
-    );
-INSERT INTO genre (libelle) VALUES ('Florent', 'floflo', '123', 'Flow');
 
-
-DROP TABLE user IF EXISTS
+DROP TABLE IF EXISTS user;
 CREATE TABLE user(
-    idUser INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    login NVARCHAR(255) NOT NULL,
-    password NVARCHAR(255) NOT NULL,
-    prenom NVARCHAR(255) NOT NULL,
-    );
-INSERT INTO genre (libelle) VALUES ('Florent', 'floflo', '123', 'Flow');
-
-DROP TABLE cheese IF EXISTS
-CREATE TABLE cheese(
-    idCheese INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nom NVARCHAR(255) NOT NULL
+     idUser INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     login NVARCHAR(255) NOT NULL,
+     password NVARCHAR(255) NOT NULL,
+     prenom NVARCHAR(255) NOT NULL
 );
-INSERT INTO cheese(nom) VALUES ('camembert')
+
+INSERT INTO user(idUSer,login,password,prenom) VALUES (1,'floflo', '123', 'Flow');
+
+DROP TABLE IF EXISTS cheese;
+CREATE TABLE cheese(
+   idCheese INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+   nom NVARCHAR(255) NOT NULL
+);
+INSERT INTO cheese(idCheese,nom) VALUES (1,'camembert');
+
+DROP TABLE IF EXISTS userCheese;
+CREATE TABLE userCheese(
+    idUser INTEGER,
+    idCheese INTEGER,
+    CONSTRAINT idUser FOREIGN KEY(idUser) REFERENCES user(idUser),
+    CONSTRAINT idCheese FOREIGN KEY(idCheese) REFERENCES cheese(idCheese)
+);
+
+INSERT INTO userCheese (idUser,idCheese) VALUES (1,1);
