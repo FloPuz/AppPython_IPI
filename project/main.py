85e17a171f925180085e4a3e5293f1ff1fb6158c
FloPuz <fpuzenat1@gmail.com>
2023-03-27 16:54:39 +0200

    Test et correction des methodes de CRUD de users

--- project/main.py
+++ project/main.py
@@ -3,7 +3,11 @@ from flask import render_template, make_response, abort, redirect, url_for
 from flask import session, request
 from markupsafe import escape
 from werkzeug.utils import secure_filename
-from project import *
+from werkzeug.security import generate_password_hash
+import sqlite3
+
+import click
+from flask import current_app, g
 
 app = Flask(__name__)
 
@@ -76,15 +80,22 @@ def insert_user(login, password, prenom):
         return error
 
 #Supprime un user de la db
-def delete_user(user)
+def delete_user(user):
     db= get_db()
     try:
-        db.execute("DELETE FROM user WHERE idUser = ?"(user['idUser'],))
+        db.execute("DELETE FROM user WHERE idUser = ?",(user['idUser'],))
         db.commit()
     except db.IntegrityError:
         error = f"User {username} doesn't exist."
         return error
 
+@app.route('/test')
+def test():
+    users = get_all_users
+    insert_user('totMusik','taratata','Florent')
+    user = get_user_by_username('totMusik')
+    delete_user(user)
+    return render_template("home.html")
 
 
 
@@ -99,3 +110,49 @@ def delete_user(user)
 
 
 
+
+# db logic code
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("data.db")
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+    with app.app_context():
+        with current_app.open_resource("cheesse.sql") as f:
+            print("ok")
+            db.executescript(f.read().decode("utf8"))
+
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+def create_app():
+    init_app(app)
+    with app.app_context():
+        db = get_db()
+        cursor = db.cursor()
+        user = cursor.execute("""SELECT * from user""").fetchone()
+        print(user["login"])
+    return app
