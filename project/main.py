7011fee3a79b0ebb6bfc6433a8b2a6f47638ae0c
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-03-27 15:19:00 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -6,36 +6,40 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/rank')
+
+@app.route("/rank")
 def rank():
-    return render_template('rank.html')
+    return render_template("rank.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def connection():
-    if request.method == 'GET':       
-        return render_template('login.html')
+    if request.method == "GET":
+        return render_template("login.html")
     else:
-        #TODO -- Check in db and add logic
+        # TODO -- Check in db and add logic
         return redirect(url_for("home"))
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-    #TODO -- remove connection from instance
+    # TODO -- remove connection from instance
     return url_for("home")
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def sign_up():
-    if request.method == 'GET':       
+    if request.method == "GET":
         return render_template("signup.html")
     else:
         print("Handle Logic Please")
-        #TODO -- Logic for sign Up
-
+        # TODO -- Logic for sign Up
 
 
 # @app.errorhandler(401)
